7ca323dea1776819179a176f7bc89d25b828cf90
unknown <akur1@yahoo.com>
2020-10-17 19:52:37 +0700

    Praktikum3

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,5 +8,12 @@ class Home extends BaseController
 	}
 
 	//--------------------------------------------------------------------
-
+	public function show()
+	{
+		$data['nama'] = 'Chandra';
+		$data['jurusan'] = 'ilkomp';
+		echo view('mahasiswa/header', $data);
+		echo view('mahasiswa/index', $data);
+		echo view('mahasiswa/footer', $data);
+	}
 }
